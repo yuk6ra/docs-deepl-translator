096aea710138e499d0426bfc1adff3c97d6812c0
yuk6ra <yuki.imamura.all@gmail.com>
2023-08-10 00:38:31 +0900

    Update main

--- main.go
+++ main.go
@@ -4,14 +4,86 @@ import (
 	"deepl/lib"
 	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
 func main() {
-	input := "docs/input/overview.md"
-	output := "docs/output/overview.md"
 
+	targetPath := "docs/"
+	
+	markdownFiles, err := getAllMarkdownFiles(targetPath)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i, mdFile := range markdownFiles {
+		if i == 1 {
+			fmt.Println(mdFile)
+			translated := translate(mdFile)
+
+			outputPath := strings.Replace(mdFile, "docs/", "docs_ja/", 1)
+
+			write(outputPath, translated)
+		}
+	}
+
+}
+
+
+func getAllMarkdownFiles(rootPath string) ([]string, error) {
+	var mdFiles []string
+
+	err := filepath.WalkDir(rootPath, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() && filepath.Ext(d.Name()) == ".md" {
+			mdFiles = append(mdFiles, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(mdFiles)
+
+	return mdFiles, nil
+}
+
+func write(filepath string, resultSlice []string) {
+
+	dir := filepath[:strings.LastIndex(filepath, "/")]
+	fmt.Println(dir)
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	file, err := os.Create(filepath)
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	data := []byte(strings.Join(resultSlice, "\n"))
+
+	_, err = file.Write(data)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+
+func translate(input string) []string {
 	str, err := os.ReadFile(input)
 
 	if err != nil {
@@ -57,6 +129,9 @@ func main() {
 		case strings.Contains(text, ":::"):
 			md := lib.ConvertMarkdown(text)
 			resultSlice = append(resultSlice, "\n"+md+"\n")
+		case strings.Contains(text, "<code"):
+			md := lib.ConvertMarkdown(text)
+			resultSlice = append(resultSlice, "\n"+md+"\n")
 		default:
 			req := lib.DeeplRequest{
 				Text:        text,
@@ -70,22 +145,5 @@ func main() {
 		}
 	}
 
-	write(output, resultSlice)
-}
-
-func write(output string, resultSlice []string) {
-	file, err := os.Create(output)
-	defer file.Close()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	data := []byte(strings.Join(resultSlice, "\n"))
-
-	_, err = file.Write(data)
-
-	if err != nil {
-		fmt.Println(err)
-	}
+	return resultSlice
 }
